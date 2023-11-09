13e58b2869c45d25c1dacb83583ac47402e3751e
Nicolas <nicolas.moyon@ynov.com>
2023-11-09 14:14:33 +0100

    working, doing the css

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 )
 
 type Promotion struct {
-	Titre       string
-	Nom         string
-	Filière     string
-	Niveau      int
-	Nbétudiants int
+	Titre   string
+	Nom     string
+	Filière string
+	Niveau  int
+	Users   []User
 }
 
 type User struct {
@@ -21,9 +22,10 @@ type User struct {
 	Sex       string
 }
 
-type Etudiant struct {
-	Titre string
-	Users []User
+type DonneesPromo struct {
+	CurrentPromo Promotion
+	NbUsers      int
+	TitreUsers   string
 }
 
 func main() {
@@ -31,33 +33,33 @@ func main() {
 
 	if err != nil {
 		fmt.Println(fmt.Sprint("ERREUR=> %s", err.Error()))
-		return
+		os.Exit(1)
 	}
 
 	http.HandleFunc("/promo", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("received request for /promo")
-		data := struct {
-			Promotion Promotion
-			Etudiant  Etudiant
-		}{
-			Promotion{
-				Titre:       "Information sur la promotion",
-				Nom:         "Mentor'ac",
-				Filière:     "Informatique",
-				Niveau:      5,
-				Nbétudiants: 3,
+		promo := Promotion{
+			Titre:   "Information sur la promotion",
+			Nom:     "Mentor'ac",
+			Filière: "Informatique",
+			Niveau:  5,
+			Users: []User{
+				{"Cyril", "RODRIGUES", 22, "Homme"},
+				{"Kheir-eddine", "MEDERREG", 22, "Homme"},
+				{"Alan", "PHILIPIERT", 26, "Homme"},
 			},
-			Etudiant{
-				Titre: "Liste des étudiants",
-				Users: []User{
-					{"Cyril", "RODRIGUES", 22, "Homme"},
-					{"Kheir-eddine", "MEDERREG", 22, "Homme"},
-					{"Alan", "PHILIPIERT", 26, "Homme"},
-				},
-			},
+		}
+		data := DonneesPromo{
+			CurrentPromo: promo,
+			NbUsers:      len(promo.Users), // Calculate the number of users dynamically
+			TitreUsers:   "Liste des étudiants",
 		}
 		temp.ExecuteTemplate(w, "promo", data)
 	})
+
+	rootDoc, _ := os.Getwd()
+	fileserver := http.FileServer(http.Dir(rootDoc))
+	http.Handle("/static/", http.StripPrefix("/static/", fileserver))
 
 	http.ListenAndServe(":8080", nil)
 }
