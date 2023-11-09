0f3e4288e2be758c48ab2d48a7819108eb4fd177
Nicolas <nicolas.moyon@ynov.com>
2023-11-09 11:10:32 +0100

    update befor test

--- main.go
+++ main.go
@@ -14,11 +14,16 @@ type Promotion struct {
 	Nbétudiants int
 }
 
+type User struct {
+	FirstName string
+	LastName  string
+	Age       int
+	Sex       string
+}
+
 type Etudiant struct {
-	Prénom string
-	Nom    string
-	Age    int
-	Sex    string
+	Titre string
+	Users []User
 }
 
 func main() {
@@ -30,15 +35,29 @@ func main() {
 	}
 
 	http.HandleFunc("/promo", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("recived request for /promo")
-		data := Promotion{
-			Titre:       "Information sur la promotion",
-			Nom:         "Mentor'ac",
-			Filière:     "Informatique",
-			Niveau:      5,
-			Nbétudiants: 3,
+		fmt.Println("received request for /promo")
+		data := struct {
+			Promotion Promotion
+			Etudiant  Etudiant
+		}{
+			Promotion{
+				Titre:       "Information sur la promotion",
+				Nom:         "Mentor'ac",
+				Filière:     "Informatique",
+				Niveau:      5,
+				Nbétudiants: 3,
+			},
+			Etudiant{
+				Titre: "Liste des étudiants",
+				Users: []User{
+					{"Cyril", "RODRIGUES", 22, "Homme"},
+					{"Kheir-eddine", "MEDERREG", 22, "Homme"},
+					{"Alan", "PHILIPIERT", 26, "Homme"},
+				},
+			},
 		}
 		temp.ExecuteTemplate(w, "promo", data)
 	})
+
 	http.ListenAndServe(":8080", nil)
 }
